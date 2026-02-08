1980e7edfe39feeb7cab51a67c62f64f25044705
Matheus Evangelista <matheuscharnas@gmail.com>
2026-02-08 19:23:02 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,6 +35,12 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 
+		System.out.println("\n=== TEST 4: seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha wayne");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+
 	}
 
 }
